188938d76ab293ec63efd39e1c484b1f7064a5d5
ElderChe <elderchegt123@gmail.com>
2026-03-01 17:45:18 -0600

    Nuevas funciones temporales
    Se crearon algunas funciones temporales que luego se implementaran como permanentes con el manejo de archivos
    Se añadop crear,listar,buscar, prestar libros, y se creo la clase libro

---
+++ ProyectoIPC/Proyecto1/src/proyecto1/Libro.java
@@ -0,0 +1,12 @@
+package proyecto1;
+
+public class Libro {
+    String codigo;
+    String isbn;
+    String titulo;
+    String autor;
+    String genero;
+    int ano;
+    int cantidadTotal;
+    int cantidadDisponible;
+}
